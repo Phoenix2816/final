be5cec03997d6fdd030a282af4394be62e7b67bd
Phoenix2816 <kirlilog@yandex.ru>
2026-07-21 01:41:20 +0500

    fix: use correct EmailJS v5 API (init object, no 4th send arg)

--- server/services/email.js
+++ server/services/email.js
@@ -5,7 +5,7 @@ const EMAILJS_TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID;
 const EMAILJS_PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY;
 const EMAILJS_PRIVATE_KEY = process.env.EMAILJS_PRIVATE_KEY;
 
-emailjs.init(EMAILJS_PUBLIC_KEY);
+emailjs.init({ publicKey: EMAILJS_PUBLIC_KEY });
 
 async function sendPasswordChangeEmail(toEmail, userName, confirmLink) {
   if (!EMAILJS_SERVICE_ID || !EMAILJS_TEMPLATE_ID || !EMAILJS_PUBLIC_KEY) {
@@ -16,15 +16,10 @@ async function sendPasswordChangeEmail(toEmail, userName, confirmLink) {
   const message = `Hello ${userName || toEmail},\n\nClick the link below to confirm your password change:\n${confirmLink}\n\nThis link will expire in 15 minutes.\n\nIf you didn't request this, ignore this email.`;
 
   try {
-    await emailjs.send(
-      EMAILJS_SERVICE_ID,
-      EMAILJS_TEMPLATE_ID,
-      {
-        message,
-        email: toEmail,
-      },
-      EMAILJS_PRIVATE_KEY
-    );
+    await emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
+      message,
+      email: toEmail,
+    });
   } catch (err) {
     console.error("Failed to send password change email:", err);
     throw new Error("Failed to send confirmation email");
